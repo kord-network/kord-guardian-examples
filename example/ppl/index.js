5a68c0a46316f0e4857af434d8815db1808bf4c3
lukehedger <luke.hedger@gmail.com>
2018-01-23 14:59:12 +0000

    Update boilerplate and examples with meta.js library

--- example/ppl/index.js
+++ example/ppl/index.js
@@ -1,15 +1,7 @@
 const {
-  bufferToHex,
-  ecrecover,
-  ecsign,
-  fromRpcSig,
-  pubToAddress,
-  sha3,
-  toBuffer,
-  toChecksumAddress,
-  toRpcSig
-} = require('ethereumjs-util')
-
+  createVerifiedIdentityClaimObject,
+  verifyIdentityClaim,
+} = require('@meta.js/identity-claims')
 const { json } = require('micro')
 const microCors = require('micro-cors')
 
@@ -22,32 +14,20 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 module.exports = cors(async (req, res) => {
-  // META Claims Service config
-  const metaClaimsService = {
+  // claim issuer
+  const issuer = {
     id: process.env.META_ID,
     privateKey: process.env.PRIVATE_KEY,
-    property: process.env.CLAIM_PROPERTY,
   }
+
+  // claim property
+  const property = process.env.CLAIM_PROPERTY
 
   // parse request body
   const { address, claimHash, claimMessage, signature, subject } = await json(req)
 
-  // generate signature parameters
-  const { v, r, s } = fromRpcSig(signature)
-
-  // generate claim buffer from claim hash minus `0x` prefix
-  const claimBuffer = Buffer.from(claimHash.substring(2), 'hex')
-
-  // recover public key from claim
-  const recoveredPublicKey = ecrecover(claimBuffer, v, r, s)
-
-  // generate Ethereum address hex from public key
-  const recoveredAddress = toChecksumAddress(
-    bufferToHex(pubToAddress(recoveredPublicKey))
-  )
-
   // verify recovered address equals given address
-  const verified = recoveredAddress === address
+  const verified = verifyIdentityClaim(address, claimHash, signature)
 
   // throw error for unverified claims
   if (!verified) return {
@@ -56,36 +36,14 @@ module.exports = cors(async (req, res) => {
     }]
   }
 
-  // set the claim value being verified
-  const verifiedClaimValue = claimMessage
-
-  // generate verified claim buffer
-  const verifiedClaimBuffer = sha3(Buffer.concat([
-    toBuffer(metaClaimsService.id),
-    toBuffer(subject),
-    toBuffer(metaClaimsService.property),
-    toBuffer(verifiedClaimValue),
-  ]))
-
-  // generate ECDSA signature of verified claim buffer using the MCS private key
-  const verifiedClaimSignatureObject = ecsign(
-    verifiedClaimBuffer,
-    Buffer.from(metaClaimsService.privateKey, 'hex')
+  // generate a verified META Identity Claim object
+  const verifiedIdentityClaim = createVerifiedIdentityClaimObject(
+    claimMessage,
+    issuer,
+    property,
+    subject
   )
 
-  // convert ECDSA signature buffer to hex value
-  const verifiedClaimSignature = toRpcSig(
-    verifiedClaimSignatureObject.v,
-    verifiedClaimSignatureObject.r,
-    verifiedClaimSignatureObject.s
-  )
-
-  // return response body
-  return {
-    claim: verifiedClaimValue,
-    issuer: metaClaimsService.id,
-    property: metaClaimsService.property,
-    signature: verifiedClaimSignature,
-    subject: subject,
-  }
+  // return verified META Identity Claim in response body
+  return verifiedIdentityClaim
 })
--- index.js
+++ index.js
@@ -1,58 +1,44 @@
 const {
-  bufferToHex,
-  ecrecover,
-  ecsign,
-  fromRpcSig,
-  pubToAddress,
-  sha3,
-  toBuffer,
-  toChecksumAddress,
-  toRpcSig
-} = require('ethereumjs-util')
-
+  createVerifiedIdentityClaimObject,
+  verifyIdentityClaim,
+} = require('@meta.js/identity-claims')
 const { json } = require('micro')
 const microCors = require('micro-cors')
 
-// configure CORS
+// Configure CORS
 const cors = microCors({ allowMethods: ['POST'] })
 
-// set env variables in non-production environments
+// Set env variables in non-production environments
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config({ path: './test/fixtures/.env' })
 }
 
 module.exports = cors(async (req, res) => {
-  // META Claims Service config
-  const metaClaimsService = {
+  // Set the claim issuer from environment variables
+  const issuer = {
     id: process.env.META_ID,
     privateKey: process.env.PRIVATE_KEY,
-    property: process.env.CLAIM_PROPERTY,
   }
 
-  // parse request body
+  // Set the claim property from environment variables
+  const property = process.env.CLAIM_PROPERTY
+
+  // Parse request body
   const { address, claimHash, claimMessage, signature, subject } = await json(req)
 
   /**
-   * Recover claim subject's address from signature
-   * @todo - Abstract this code to a library
+   * Verify claim sender's identity
    */
 
-  // generate signature parameters
-  const { v, r, s } = fromRpcSig(signature)
+  // Recover claim subject's address from signature and verify recovered address
+  // matches given address
+  const verified = verifyIdentityClaim(address, claimHash, signature)
 
-  // generate claim buffer from claim hash minus `0x` prefix
-  const claimBuffer = Buffer.from(claimHash.substring(2), 'hex')
+  /**
+   * Insert additional verification
+   */
 
-  // recover public key from claim
-  const recoveredPublicKey = ecrecover(claimBuffer, v, r, s)
-
-  // generate Ethereum address hex from public key
-  const recoveredAddress = toChecksumAddress(
-    bufferToHex(pubToAddress(recoveredPublicKey))
-  )
-
-  // verify recovered address equals given address
-  const verified = recoveredAddress === address
+  // ...
 
   /**
    * Claim verification failed
@@ -69,36 +55,14 @@ module.exports = cors(async (req, res) => {
    * Claim verification passed
    */
 
-  // set the claim value being verified
-  const verifiedClaimValue = claimMessage
-
-  // generate verified claim buffer
-  const verifiedClaimBuffer = sha3(Buffer.concat([
-    toBuffer(metaClaimsService.id),
-    toBuffer(subject),
-    toBuffer(metaClaimsService.property),
-    toBuffer(verifiedClaimValue),
-  ]))
-
-  // generate ECDSA signature of verified claim buffer using the MCS private key
-  const verifiedClaimSignatureObject = ecsign(
-    verifiedClaimBuffer,
-    Buffer.from(metaClaimsService.privateKey, 'hex')
+  // Generate a verified META Identity Claim object
+  const verifiedIdentityClaim = createVerifiedIdentityClaimObject(
+    claimMessage,
+    issuer,
+    property,
+    subject
   )
 
-  // convert ECDSA signature buffer to hex value
-  const verifiedClaimSignature = toRpcSig(
-    verifiedClaimSignatureObject.v,
-    verifiedClaimSignatureObject.r,
-    verifiedClaimSignatureObject.s
-  )
-
-  // return response body
-  return {
-    claim: verifiedClaimValue,
-    issuer: metaClaimsService.id,
-    property: metaClaimsService.property,
-    signature: verifiedClaimSignature,
-    subject: subject,
-  }
+  // Return verified META Identity Claim in response body
+  return verifiedIdentityClaim
 })
